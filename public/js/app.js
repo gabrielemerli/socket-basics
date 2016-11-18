501ba753bffb55305dd0372d8807c53efc115492
Gabriele Merli <gabrielemerli@yahoo.it>
2016-11-18 22:50:09 +0100

    Added custom message event

--- public/js/app.js
+++ public/js/app.js
@@ -5,4 +5,12 @@ var socket = io();
 socket.on('connect', function() {
 	//Questo lo vedi nella console delle developer tools
 	console.log("Connected to socket.io server");
+
+});
+
+
+//Ora ascolto gli eventi in arrivo e faccio qualcosa quando li ricevo
+socket.on('messaggio', function(messaggio_ricevuto) {
+	console.log('Nuovo messaggio ricevuto: ');
+	console.log(messaggio_ricevuto.testo);
 });
--- server.js
+++ server.js
@@ -11,10 +11,35 @@ var io = require('socket.io')(http);
 app.use(express.static(__dirname+'/public'));
 
 //On -> Ascolta eventi ('event_name')
-io.on('connection', function(){
+io.on('connection', function(socket){
 	console.log('User connected via socket.io');
-})
+
+	//QUesto per far parlare 2 client tra di loro
+	//Devo quindi emettere tutti i messaggi che arrivano al server
+	//Mi metto in ascolto per i messaggi e poi
+	socket.on('messaggio', function(messaggio){
+		//Li loggo sulla console di nodejs (sul server) e poi
+		console.log('Messaggio ricevuto'+messaggio.testo);
+		//Li emetto a tutti tranne che quello che l'ha mandato , se avessi voluto
+		//mandarli a tutti compreso chi l'ha mandato avrei dovuto scrivere
+		//io.emit
+		socket.broadcast.emit('messaggio', messaggio);
+
+		//Poi per provare apro 2 browser che puntano qui.
+		//Sulla console di uno dei 2 scrivo
+		//socket.emit('messaggio', {testo: 'due dita su per il culo'})
+		//E mi compare sulla console del server e dell'altro client
+	});
+
+	//Mando un messaggio al client
+	socket.emit('messaggio', {
+		testo: 'Wealcome to the metal age'
+	});
+
+
+
+});
 
 http.listen(PORT, function() {
 	console.log('Server started!');
-})
+});
