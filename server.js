25fa9d448b080297eaa02ad367b39f018b81f374
Gabriele Merli <gabrielemerli@yahoo.it>
2016-11-18 22:05:31 +0100

    Add socket.io client and server

--- server.js
+++ server.js
@@ -5,8 +5,15 @@ var app = express();
 //Start a news server e usa questa express app
 var http = require('http').Server(app);
 
+var io = require('socket.io')(http);
+
 //Voglio esporre una cartella
 app.use(express.static(__dirname+'/public'));
+
+//On -> Ascolta eventi ('event_name')
+io.on('connection', function(){
+	console.log('User connected via socket.io');
+})
 
 http.listen(PORT, function() {
 	console.log('Server started!');
